e576936fe9bcf1dcf2fbc1caaf7806fc29cf1e9c
vashuchouhan892-debug <vashuchouhan892@gmail.com>
2026-08-02 00:38:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("LOKI AI Started");
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("LOKI AI Started");
+
+  const title = document.querySelector("h1");
+
+  setInterval(() => {
+    title.style.textShadow =
+      Math.random() > 0.5
+        ? "0 0 30px cyan"
+        : "0 0 15px #00ffff";
+  }, 500);
+});
